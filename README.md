001aecc51ac8b3fa9141dedcda324790bdd17007
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2026-01-09 21:00:12 +0530

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # pterodactyl-egg
-Use this to get an egg, not the kind u eat, the kind u put in pterodactyl panel to get a server
+Use this to get an egg, not the kind u eat, the kind u put in pterodactyl panel to get a Bombsquad server
+
+## How to configure the egg
+- Select a nest or create a new one
+- Create a new egg
+- Enter the following info in the mentioned field
+  - Docker Images: ghcr.io/bombsquad-community/pterodactyl-egg/bombsquad_egg:debug_v0.0.4alpha_x86_64
+  - Startup Command: ./ballisticakit_server
+  - Stop Command: ^C
+  - Log Configuration: {}
+  - Configuration Files: {}
+  - Start Configuration: {}
+- Click "Create"
+
+## How to get bombsquad server files
+You can use install-script.sh and paste in the "Install Script" tab in your egg to install the vanilla server
+or customize it to install custom server scripts
+or create a server, and after starting it, manually upload the server files
+
+Congratulations, your egg is created
+
---
+++ install-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash                                                                   
+# Pterodactyl BombSquad Install Script                                        
+                                                                              
+apt update                                                                    
+apt install -y curl tar jq                                                    
+                                                                              
+cd /mnt/server                                                                
+                                                                              
+# Determine Architecture                                                      
+ARCH=$(uname -m)                                                              
+if [ "${ARCH}" == "x86_64" ]; then                                            
+    DOWNLOAD_URL="https://github.com/efroemling/ballistica/releases/latest/do 
+wnload/linux_x86_64_server_debug.tar"                                         
+elif [ "${ARCH}" == "aarch64" ]; then                                         
+    DOWNLOAD_URL="https://github.com/efroemling/ballistica/releases/latest/do 
+wnload/linux_arm64_server_debug.tar"                                          
+else                                                                          
+    echo "Unsupported architecture: ${ARCH}"                                  
+    exit 1                                                                    
+fi                                                                            
+                                                                              
+echo "Downloading BombSquad Server from ${DOWNLOAD_URL}..."                   
+curl -L -o server.tar ${DOWNLOAD_URL}                                         
+                                                                              
+echo "Extracting files..."                                                    
+tar -xf server.tar --strip-components=5 "build/prefab/full/linux_${ARCH/aarch 
+64/arm64}_server/debug/"                                                      
+                                                                              
+# Clean up                                                                    
+rm server.tar                                                                 
+echo "Install complete."
